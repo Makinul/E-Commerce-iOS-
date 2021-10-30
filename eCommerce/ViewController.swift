3dd18c14dab909da177f4a521f9cde80406fa592
RavenSystemsLtd <rslsbanalytics@gmail.com>
2021-10-30 17:18:47 +0600

    Primary testing

--- eCommerce/ViewController.swift
+++ eCommerce/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("View controller")
     }
 
+    @IBAction func buttonClicked(_ sender: UIButton) {
+      print("This is a Git tutorial")
+    }
 
 }
 
